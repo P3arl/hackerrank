9d7d24c483123e513de55f758015124f92460a3e
Parveen Parmar <parveenparmar03@gmail.com>
2017-09-29 11:07:31 +0530

    Update Solution.java
    removed unnecessary curr variable

--- Delete duplicate-value nodes from a sorted linked list/Solution.java
+++ Delete duplicate-value nodes from a sorted linked list/Solution.java
@@ -5,13 +5,10 @@ class Solution{
          return null;
       }
       Node temp = head;
-      Node curr = head;
       while(temp.next!=null) {
          if(temp.data==temp.next.data) {
             temp.next=temp.next.next;
-            curr = temp;
          }else {
-            curr = curr.next;
             temp = temp.next;
          }
       }
