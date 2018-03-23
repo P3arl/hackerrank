ab28c7c262e5569a027bebedcee59bb7caf084e2
Parveen Parmar <parveenparmar03@gmail.com>
2018-03-23 14:36:08 +0530

    Create Solution2.java

---
+++ Alternating Characters/Solution2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static int alternatingCharacters(String s){
+		int deletions = 0;
+		for(int i=1;i<s.length();i++) {
+			if(s.charAt(i-1) == s.charAt(i)) {
+				deletions++;
+			}
+		}
+		return deletions;
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for(int a0 = 0; a0 < q; a0++){
+            String s = in.next();
+            int result = alternatingCharacters(s);
+            System.out.println(result);
+        }
+    }
+}
